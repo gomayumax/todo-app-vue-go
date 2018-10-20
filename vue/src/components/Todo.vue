c074b83fa53369291ed320d4a1c049e288a6a0d6
ごま <gomayumax@users.noreply.github.com>
2018-10-20 23:55:10 +0900

    add Todo component

---
+++ vue/src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <form>
+            <input type="text"/>
+            <button>add</button>
+        </form>
+        <ul>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo'
+}
+</script>
+
+<style scoped>
+
+</style>
